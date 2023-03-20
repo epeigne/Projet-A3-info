33d09e9e55174b3c0ed4908a99b1fbc0ddaabbbb
epeigne <peigne.enzo@gmail.com>
2023-03-20 17:16:18 +0100

    consigne 100%

--- code/consigne.c
+++ code/consigne.c
@@ -4,7 +4,29 @@
    {
    
       float thermostat_f=0;
-   
+
+      if(access(".verrouConsigne", F_OK) != 0){
+
+         FILE* readFile = fopen("consigne.txt", "r");
+
+         if (readFile == NULL) //check if consigne.txt exists
+         {
+            printf("Fichier consigne.txt inexistant\n");
+            return 0;
+         }
+
+         fscanf(readFile, "%f", &thermostat_f);
+         fclose(readFile);
+
+         FILE* lockfile = fopen(".verrouConsigne", "w");
+         fclose(lockfile);
+         remove(".verrouConsigne"); //remove .verrouConsigne
+      }
+      else{
+         return thermostatPrec_f;
+      }
+
+         
      
       return thermostat_f;
    
--- code/consigne.h
+++ code/consigne.h
@@ -2,7 +2,8 @@
 #define CONSIGNE_H
 #include <stdio.h>
 #include <stdlib.h>
-#include "define.h" 
+#include "define.h"
+#include <unistd.h>
 
 // Pensez à bien proteger vos .h avec des ifndef
 
