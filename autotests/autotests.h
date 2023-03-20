10fd9d5a73cfe4014ec3edd7dc2825578db000f8
epeigne <peigne.enzo@gmail.com>
2023-03-20 13:53:22 +0100

    update

--- autotests/autotests.h
+++ autotests/autotests.h
@@ -1,6 +1,10 @@
+#ifndef AUTOTESTS_H
+#define AUTOTESTS_H
 
 float testConsigne();
 float testVisualisationT();
 float testVisualisationC();
 float testRegulationTOR();
 float testRegulationPID();
+
+#endif
