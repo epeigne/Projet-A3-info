af9744687448bd9c8f556b37840195cf5b3db7c2
Mickaeln44 <mickael.neroda@gmail.com>
2023-03-22 09:03:01 +0100

    1

--- code/regulation.h
+++ code/regulation.h
@@ -1,12 +1,13 @@
 #ifndef REGULATION_H
 #define REGULATION_H
-    #include <stdio.h>
-	#include <stdlib.h>
-	#include "define.h" 
+#include <stdio.h>
+#include <stdlib.h>
+#include "define.h" 
     
    // Pensez à bien proteger vos .h avec des ifndef
 	
-   float regulationTest(int regul,float consigne,float* tabT, int nT);
+float regulationTest(int regul,float consigne,float* tabT, int nT);
 float regulation(float consigne, float* tabT, int nT);
+float regulation_PID(float consigne, float* tabT, int nT);
 
-   #endif
+#endif
