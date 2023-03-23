7219851dfdcd1ff276ad1657204f29cc8d762ac1
epeigne <peigne.enzo@gmail.com>
2023-03-23 11:40:39 +0100

    comment regul

--- code/regulation.c
+++ code/regulation.c
@@ -19,9 +19,9 @@ float regulationTest(int regul,float consigne,float* tabT, int nT){
 	}
 	
 	if (regul == 2){ // PID
-		float integral = 0;
-		float previousE = 0;
-		float previousConsigne = 0;
+		float integral = 0; // integral de l'erreur pour la regulation PID
+		float previousE = 0; // ecart precedente pour la regulation PID
+		float previousConsigne = 0; // consigne precedente pour la regulation PID
 		
 		for (int i = 0; i < nT; i++)
 		{
@@ -40,13 +40,13 @@ float regulationPID(float consigne, float Tint, float* I, float* previousE, floa
 	
 	// intergral term 
 	if (*previousE < e){
-		*I += K_I * (DELTA_T * *previousE + (DELTA_T * (e - *previousE)) / 2);
+		*I += K_I * (DELTA_T * *previousE + (DELTA_T * (e - *previousE)) / 2); // trapezoidal rule
 	} else {
-		*I += K_I * (DELTA_T * e + (DELTA_T * (*previousE - e)) / 2);
+		*I += K_I * (DELTA_T * e + (DELTA_T * (*previousE - e)) / 2); // trapezoidal rule
 	}
 	
 	// derivate term
-	float D = K_D * (e - *previousE) / DELTA_T;
+	float D = K_D * (e - *previousE) / DELTA_T; 
 
 	if (consigne != *previousConsigne){ // reseting integral and derivate value if consigna has changed or if it is the first iteration
 	    *I = 0;
